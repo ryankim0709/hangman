31277e64d6a16d67a99ed3b7314a1abf60cc91c8
Ryan Kim <ryankim0709@gmail.com>
2022-07-27 17:34:33 -0700

    hangman

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [word, setWord] = useState("");
+  const [guess, setGuess] = useState("");
+  const [numGuesses, setNumGuesses] = useState(0);
+  const [currState, setCurrState] = useState("");
+  function checkGuess(guess) {
+    var currstatus = currState;
+    if (word.includes(guess)) {
+      for (var i = 0; i < word.length - guess.length + 1; i++) {
+        if (word.substring(i, i + guess.length) == guess) {
+          for (var j = i; j < i + guess.length; j++) {
+            currstatus[j] = guess[j - i];
+          }
+        }
+      }
+      if (!currstatus.includes("_ ")) {
+        setNumGuesses(0);
+        setWord("");
+        setGuess("");
+        setCurrState("");
+        alert("Congrats! You won :)");
+      } else {
+        setCurrState(currstatus);
+      }
+    } else {
+      setNumGuesses(numGuesses + guess.length);
+      if (numGuesses >= 4) {
+        setWord("");
+        setGuess("");
+        setNumGuesses(0);
+        setCurrState("");
+        alert("Sorry! You lost :(");
+      }
+    }
+    setGuess("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex items-center justify-center flex-col">
+      {word && (
+        <div class="flex mt-1 border-4">
+          <img src={require(`./images/stage${numGuesses}.png`)} />
+        </div>
+      )}
+
+      <div class="flex mt-2 flex-row w-2/5 justify-around">
+        <input
+          class="border rounded w-1/2 text-gray-700"
+          type="text"
+          placeholder="Guess"
+          value={guess}
+          onChange={(event) => {
+            setGuess(event.target.value);
+          }}
+        />
+        {!word && (
+          <button
+            onClick={() => {
+              fetch("https://random-word-api.herokuapp.com/word")
+                .then((res) => res.json())
+                .then((word) => {
+                  console.log(word[0]);
+                  setWord(word[0]);
+                  setNumGuesses(0);
+
+                  var temp = [];
+                  for (var i = 0; i < word[0].length; i++) {
+                    temp.push("_ ");
+                  }
+                  setCurrState(temp);
+                });
+            }}
+          >
+            Play new game
+          </button>
+        )}
+        {word && (
+          <button
+            onClick={() => {
+              checkGuess(guess);
+            }}
+          >
+            Guess
+          </button>
+        )}
+      </div>
+      <div class="mt-1">
+        <text>{currState}</text>
+      </div>
     </div>
   );
 }
-
-export default App;
